8ce9f06fedb42f76401ebd8951bb206d9eb5291e
thomas@smov.se <jstSims99>
2016-06-18 22:15:31 +0200

    Spawn virker :)

--- src/me/nipe/Bil/Bil.java
+++ src/me/nipe/Bil/Bil.java
@@ -1,9 +1,12 @@
 package me.nipe.Bil;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 /**
  * Created by Mads og Thomas on 17/06/2016.
@@ -24,21 +27,21 @@ public class Bil extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String comandLabel, String [] args) {
-        if ((cmd.getName().equalsIgnoreCase("uc") || cmd.getName().equalsIgnoreCase("undercover")) && sender instanceof Player && sender.isOp()) {
+        if (cmd.getName().equalsIgnoreCase("bil") && sender instanceof Player && sender.isOp()) {
             Player player = (Player) sender;
-            player.setPlayerListName("Undercover");
-            return true;
-        } else if ((cmd.getName().equalsIgnoreCase("unuc") || cmd.getName().equalsIgnoreCase("unundercover")) && sender instanceof Player && sender.isOp()) {
-            Player player = (Player) sender;
-            player.setPlayerListName(player.getName());
-            return true;
-        } else if (cmd.getName().equalsIgnoreCase("nick") && sender instanceof Player && sender.isOp()) {
-            if (args.length != 1) {
-                sender.sendMessage("Du skal skrive et navn");
-                return false;
-            }
-            Player player = (Player) sender;
-            player.setPlayerListName(args[0]);
+
+            // Hvor er jeg:
+            Location eyelocation = player.getEyeLocation();
+
+            // Hvilken vej vender jeg:
+            Vector vec = player.getLocation().getDirection();
+
+            // Find placeringen foran mig:
+            Location frontlocation = eyelocation.add(vec);
+
+            // Spawn et eller andet foran mig:
+            player.getWorld().spawnEntity(frontlocation, EntityType.MINECART);
+
             return true;
         }
         return false;
